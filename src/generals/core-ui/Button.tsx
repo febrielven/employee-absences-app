5a68b4a5aa4e37c18b60098915015b7cd64598dc
unknown <febrianto.febri@bfi.co.id>
2021-01-22 20:25:46 +0700

    update button

--- src/generals/core-ui/Button.tsx
+++ src/generals/core-ui/Button.tsx
@@ -1,67 +1,13 @@
 import React from 'react';
 import {View} from 'react-native';
-
 import MaterialButton from '@material-ui/core/Button';
 
-
-// type Props =
-//   ButtonProps & {
-//     onPress?: () => void;
-//     // buttonType?: ButtonType;
-//     isLoading?: boolean;
-//     disabled?: boolean;
-//     containerStyle?: ViewStyle;
-//     text?:string;
-//   };
-
 function Button() {
-//   let {
-//     children,
-//     classes,
-//     isLoading,
-//     disabled,
-//     onPress,
-//     buttonType = 'default' as ButtonType,
-//     containerStyle,
-//     ...otherProps
-//   } = props;
-
-
-  return (
+ return (
     <View>
-      <MaterialButton variant="contained">Test</MaterialButton>
+      <MaterialButton variant="contained">Login</MaterialButton>
     </View>
   );
 }
-
-// const styles = createStyles({
-//   coreStyle: {
-//     fontWeight: 'bold',
-//   },
-//   default: {
-//     borderColor: BUTTON.default.border,
-//     backgroundColor: BUTTON.default.background,
-//     color: BUTTON.default.text,
-//     border: '1px solid',
-//     '&:hover': {
-//       backgroundColor: BUTTON.default.hover,
-//     },
-//   },
-//   primary: {
-//     borderColor: BUTTON.primary.border,
-//     backgroundColor: BUTTON.primary.background,
-//     border: '1px solid',
-//     '&:hover': {
-//       backgroundColor: BUTTON.primary.hover,
-//     },
-//   },
-//   secondary: {
-//     backgroundColor: BUTTON.secondary.background,
-//     color: BUTTON.secondary.text,
-//     '&:hover': {
-//       backgroundColor: BUTTON.secondary.hover,
-//     },
-//   },
-// });
 
 export default Button;
