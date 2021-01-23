2a23becee1009d5f54c40be30e5ab7853621f301
unknown <febrianto.febri@bfi.co.id>
2021-01-23 19:05:38 +0700

    in progress fixbug input

--- src/generals/core-ui/InputText.tsx
+++ src/generals/core-ui/InputText.tsx
@@ -1,17 +1,16 @@
-import React, {forwardRef, ReactNode, Ref, ChangeEvent} from 'react';
-import {StyleProp, TextStyle} from 'react-native';
-import {TextField} from '@material-ui/core';
+import React, {forwardRef, ReactNode, Ref} from 'react';
+import {TextFieldProps} from '@material-ui/core';
+import {TextInput as TextField} from 'react-native-paper';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 import {TEXT_INPUT} from '../constants/colors';
 
-export type TextInputCoreUIProps = {
+export type TextInputCoreUIProps = TextFieldProps & {
   onChangeText?: (input: string) => void;
   label?: string;
   leftElement?: ReactNode;
   rightElement?: ReactNode;
   readOnly?: boolean;
-  disabled?: boolean;
-  style:StyleProp<TextStyle>
 };
 
 function TextInput(props: TextInputCoreUIProps, ref: Ref<HTMLDivElement>) {
@@ -26,21 +25,32 @@ function TextInput(props: TextInputCoreUIProps, ref: Ref<HTMLDivElement>) {
     ...otherProps
   } = props;
 
+  // let combinedStyle = {
+  //   ...(disabled ? {backgroundColor: TEXT_INPUT.disabled} : {}),
+  //   ...style,
+  // };
+
   return (
     <TextField
-      mode='outlined'
       label={label}
       ref={ref}
       variant="outlined"
       onChange={
         onChangeText
-          ? (event:any) => {
+          ? (event) => {
               onChangeText && onChangeText(event.target.value);
             }
           : undefined
       }
-      style={style}
       disabled={disabled || readOnly}
+      InputProps={{
+        startAdornment: leftElement ? (
+          <InputAdornment position="start">{leftElement}</InputAdornment>
+        ) : undefined,
+        endAdornment: rightElement ? (
+          <InputAdornment position="end">{rightElement}</InputAdornment>
+        ) : undefined,
+      }}
       {...otherProps}
     />
   );
